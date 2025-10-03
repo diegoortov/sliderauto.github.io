3414f560766f1fd8e21c69afba70e27a2854679f
diegoortov <diegoortizt2008@gmail.com>
2025-10-03 14:04:38 -0700

    Update script.js

--- docs/script.js
+++ docs/script.js
@@ -1,1 +1,18 @@
+const imagenElemento = document.getElementById('carrusel-imagen');
+const imagenes = [
+    'imagen1.png',
+    'imagen2.png',
+    'imagen3.png'
+];
 
+let indiceActual = 0; /* variable inicializada en 0 */
+function cambiarImagen() { /* función */
+    imagenElemento.style.opacity = 0;
+    setTimeout(() => {
+        indiceActual = (indiceActual + 1) % imagenes.length;  // ciclo
+        imagenElemento.src = imagenes[indiceActual];
+        imagenElemento.style.opacity = 1;
+    }, 500);
+}
+
+setInterval(cambiarImagen, 3000); // milisegundos
